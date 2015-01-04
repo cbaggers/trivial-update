301cac6c71047a3216a27bd3a10690bb4da317f4
mrkkrp <markkarpov@opmbx.org>
2015-01-05 00:31:35 +0600

    minor edit

--- trivial-update.lisp
+++ trivial-update.lisp
@@ -24,9 +24,9 @@
 ;;; OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 ;;; WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-(cl:defpackage :trivial-update
-  (:use        #:common-lisp)
-  (:export     #:update))
+(defpackage :trivial-update
+  (:use     #:common-lisp)
+  (:export  #:update))
 
 (in-package #:trivial-update)
 
